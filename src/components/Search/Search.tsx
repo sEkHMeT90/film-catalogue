22cbd3b7a3309a5954c5f2fdce1af1597983a274
Raquel Llorens Gambin <r.llorens90@gmail.com>
2022-11-06 13:35:12 +0100

    Fix search translations

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,34 +1,42 @@
 import React from "react";
 import { TextField, Box } from "@mui/material";
+import { useIntl } from "react-intl";
 
 type Props = {
   searchQuery: string;
   setSearchQuery: (searchQuery: string) => void;
 };
 
-const Search = ({ searchQuery, setSearchQuery }: Props) => (
-  <Box
-    sx={{ p: 5 }}
-    justifyContent={"center"}
-    alignItems='center'
-    display={"flex"}
-  >
-    <form>
-      <TextField
-        id='search'
-        className='text'
-        defaultValue={searchQuery}
-        onChange={(e) => {
-          const target = e.target as HTMLTextAreaElement;
-          setSearchQuery(target.value);
-        }}
-        label='Enter a film to search'
-        variant='outlined'
-        placeholder='Search...'
-        size='small'
-      />
-    </form>
-  </Box>
-);
+const Search = ({ searchQuery, setSearchQuery }: Props) => {
+  const { formatMessage } = useIntl();
+  return (
+    <Box
+      sx={{ p: 5 }}
+      justifyContent={"center"}
+      alignItems='center'
+      display={"flex"}
+    >
+      <form>
+        <TextField
+          id='search'
+          className='text'
+          defaultValue={searchQuery}
+          onChange={(e) => {
+            const target = e.target as HTMLTextAreaElement;
+            setSearchQuery(target.value);
+          }}
+          label={formatMessage({
+            id: "filmsList.search",
+          })}
+          variant='outlined'
+          placeholder={formatMessage({
+            id: "filmsList.enterFilmSearch",
+          })}
+          size='small'
+        />
+      </form>
+    </Box>
+  );
+};
 
 export default Search;
